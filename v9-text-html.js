af6b29409675065bb2e92f9e4eb879323f32704e
Vic Chimenti <victor.chimenti@gmail.com>
2022-10-25 12:52:50 -0700

    udr

---
+++ v9-text-html.js
@@ -0,0 +1,16 @@
+/***
+*       @author Victor Chimenti, MSCS
+*       @file v9-text-html.js
+*           Button Group
+*           ID: 5555
+*           v9/text/html
+*
+*       This content type replaces the depreciated TinyMCE Editor Button Plugin.
+*       It will allow the user to create up to 4 buttons that will flex wrap
+*       responsively. The user will choose between the three existing types
+*       of brand approved buttons.
+*
+*       Document will write once when the page loads
+*
+*       @version 1.0
+*/
